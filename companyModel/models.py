a0e22814e45e95593bf74863e2ba4c63b699d4c2
Ganibalx <ganibal1993@gmail.com>
2023-10-23 01:33:36 +0500

    строительство2

--- companyModel/models.py
+++ companyModel/models.py
@@ -1,68 +1,35 @@
 from enum import Enum
 
 
-class Human:
-    """Базовый класс для людей в модели"""
-    busy = 0
-
-    def __init__(self, name, lastname):
-        self.name = name
-        self.lastname = lastname
+class UserException(Exception):
+    def __init__(self, msg):
+        super.__init__(msg)
 
 
 class Warehouse:
-    """Это склад"""
-    instance = None
+    def __init__(self, items: dict):
+        self._items = items
 
-    def __new__(cls):
-        if not cls.instance:
-            cls.instance = super().__new__(cls)
-            cls.instance.tooling = {}
-            cls.instance.materials = {}
-        return cls.instance
-
-    def add_materials(self, materials):
-        for key in materials.keys():
-            if key not in self.materials.keys():
-                self.materials[key]=materials[key]
-            else:
-                self.materials[key] = self.materials[key] + materials[key]
-
-    def add_tooling(self, tooling):
-        for key in tooling.keys():
-            if key not in self.materials.keys():
-                self.tooling[key]=tooling[key]
-            else:
-                self.tooling[key] = self.tooling[key] + tooling[key]
-
-    def issue_materials(self, key, kol):
-        if key in self.materials.keys():
-            if self.materials[key] >= kol:
-                self.materials[key] = self.materials[key] - kol
-            else:
-                raise Exception(f'не хватает{key}, доступно {self.materials[key]}')
-        else:
-            raise Exception(f'нет такого материала на складе')
-
-    def issue_tooling(self, key, kol):
-        if key in self.tooling.keys():
-            if self.tooling[key] >= kol:
-                self.tooling[key] = self.tooling[key] - kol
-            else:
-                raise Exception(f'не хватает{key}, доступно {self.tooling[key]}')
-        else:
-            raise Exception(f'нет такого инструмента на складе')
+    def get_items(self, item, count):
+        if item not in self._items:
+            raise UserException(f'Нет {item}')
+        if self._items[item] < count:
+            raise UserException(f'Не достаточное кол-во на складе {item}')
+        self._items[item] -= count
 
 
-class Customer(Human):
+class Customer:
     """Класс заказчика"""
-    def __init__(self, name, lastname, project):
-        super().__init__(name, lastname)
-        self.project = project
+    def __init__(self, project):
+        self._project = project
+
+    def get_project(self):
+        return self._project
 
 
-class Worker(Human):
-    tooling = None
+class Worker:
+    def __init__(self, tooling):
+        self.tooling = tooling
 
     def build(self):
         if self.tooling[0] == 1:
@@ -71,87 +38,37 @@ class Worker(Human):
             return False
 
 
-class Foreman(Human):
-    ongoing_project = None
-    need_to_get_materials = {}
-    need_to_get_tooling = {}
-    brigade = []
+class Foreman:
 
-    def add_to_brigade(self, worker, tooling):
-        if isinstance(worker, Worker):
-            if self.ongoing_project and not worker.busy:
-                self.brigade.append(worker)
-                worker.tooling = [0, tooling]
-                worker.busy = 1
-                if tooling in self.need_to_get_tooling.keys():
-                    self.need_to_get_tooling[tooling] = self.need_to_get_tooling[tooling] + 1
-                else:
-                    self.need_to_get_tooling[tooling] = 1
-            else:
-                raise Exception('Нет проекта или работник уже занят')
-        else:
-            raise Exception('Не передан рабочий')
+    def __init__(self, need_to_get_items):
+        self._need_to_get_items = need_to_get_items
+        self._brigade = []
 
-    def get_mat_tol(self, wh):
-        if isinstance(wh, Warehouse):
-            for k, v in self.need_to_get_materials.items():
-                wh.issue_materials(k, v)
-            self.need_to_get_materials = None
-            for k, v in self.need_to_get_tooling.items():
-                wh.issue_tooling(k, v)
-                for worker in self.brigade:
-                    if worker.tooling[1] == k and worker.tooling[0] == 0:
-                        worker.tooling[0] = 1
-            self.need_to_get_tooling = None
+    def add_worker_in_brigade(self, tooling):
+        self._brigade.append(Worker(tooling))
+        self._need_to_get_items[tooling] = self._need_to_get_items.setdefault(tooling, 0) + 1
+
+    def get_items_in_wh(self, wh: Warehouse):
+        if not isinstance(wh, Warehouse):
+            raise UserException('это не склад')
+        for k, v in self._need_to_get_items.items():
+            wh.get_items(k, v)
+        self._need_to_get_items = None
+        for worker in self._brigade:
+            worker.tooling[0] = 1
 
     def build(self):
-        if not self.need_to_get_tooling and not self.need_to_get_materials:
-            if all([i.build() for i in self.brigade]):
-                for worker in self.brigade:
-                    worker.busy = 0
-                self.busy = 0
-                self.brigade = []
-                return True
+        if self._need_to_get_items:
             return False
-        return False
-
-class Manager(Human):
-    foreman_project = None
-
-    def accept_customer(self, *, customer, foreman):
-        if isinstance(customer, Customer) and isinstance(foreman, Foreman):
-            if customer.busy == foreman.busy == self.busy == 0:
-                customer.busy = foreman.busy = self.busy = 1
-                foreman.ongoing_project = customer.project
-                self.foreman_project = {foreman.lastname: {}}
-            else:
-                raise Exception(f"кто то занят другим делом")
-        else:
-            raise Exception("Требуется передача других параметров")
-
-    def counting_material(self, foreman, materials):
-        self.foreman_project[foreman.lastname] = materials
-
-    def get_materials(self, foreman):
-        if isinstance(foreman, Foreman):
-            if foreman.lastname in self.foreman_project.keys():
-                foreman.need_to_get_materials = self.foreman_project[foreman.lastname]
-                self.foreman_project = None
-                self.busy = 0
+        return all([i.build() for i in self._brigade])
 
 
-class Posts(Enum):
-    """Должности в организации"""
-    manager = Manager
-    foreman = Foreman
-    worker = Worker
+class Manager:
+
+    def __init__(self, project):
+        self.project = project
+
+    def start_project(self, material: dict):
+        return Foreman(material)
 
 
-class Create_people:
-    def __new__(cls, *, post=None, name, lastname, project=None):
-        if post:
-            return Posts[post].value(name, lastname)
-        elif project:
-            return Customer(name, lastname, project)
-        else:
-            raise Exception("Что за хуйня?")
--- companyModel/test.py
+++ companyModel/test.py
@@ -1,21 +1,29 @@
-from models import *
-#создаем людей
-customer = Create_people(name='Петр', lastname='Иванов', project='Строим дом')
-manager = Create_people(post='manager', name='Илья', lastname='Сидоров')
-foreman = Create_people(post='foreman', name='Виталя', lastname='Филимонов')
-worker1 = Create_people(post='worker', name='Никита', lastname='Носков')
-worker2 = Create_people(post='worker', name='Денис', lastname='Прошкин')
-worker3 = Create_people(post='worker', name='Андрей', lastname='Лузин')
-#заполняем склад
-wh = Warehouse()
-wh.add_materials({'кирпич': 10, 'шифер': 20, 'линолеум': 5})
-wh.add_tooling({'молоток': 5, 'дрель': 2, 'трактор': 1, 'гвоздик': 20})
-#последовательность строительства
-manager.accept_customer(customer=customer, foreman=foreman)
-manager.counting_material(foreman, {'кирпич': 3, 'шифер': 5, 'линолеум': 1})
-manager.get_materials(foreman)
-foreman.add_to_brigade(worker1, 'молоток')
-foreman.add_to_brigade(worker2, 'трактор')
-foreman.add_to_brigade(worker3, 'гвоздик')
-foreman.get_mat_tol(wh)
-print(f'дом построен? {foreman.build()}')
+# from models import *
+# #создаем людей
+# customer = Create_people(name='Петр', lastname='Иванов', project='Строим дом')
+# manager = Create_people(post='manager', name='Илья', lastname='Сидоров')
+# foreman = Create_people(post='foreman', name='Виталя', lastname='Филимонов')
+# worker1 = Create_people(post='worker', name='Никита', lastname='Носков')
+# worker2 = Create_people(post='worker', name='Денис', lastname='Прошкин')
+# worker3 = Create_people(post='worker', name='Андрей', lastname='Лузин')
+# #заполняем склад
+# wh = Warehouse()
+# wh.add_materials({'кирпич': 10, 'шифер': 20, 'линолеум': 5})
+# wh.add_tooling({'молоток': 5, 'дрель': 2, 'трактор': 1, 'гвоздик': 20})
+# #последовательность строительства
+# manager.accept_customer(customer=customer, foreman=foreman)
+# manager.counting_material(foreman, {'кирпич': 3, 'шифер': 5, 'линолеум': 1})
+# manager.get_materials(foreman)
+# foreman.add_to_brigade(worker1, 'молоток')
+# foreman.add_to_brigade(worker2, 'трактор')
+# foreman.add_to_brigade(worker3, 'гвоздик')
+# foreman2 = Create_people(post='foreman', name='Виталя2', lastname='Филимонов2')
+# print(foreman.ongoing_project, foreman2.ongoing_project)
+# Foreman.ongoing_project = True
+# print(foreman.ongoing_project, foreman2.ongoing_project)
+# foreman.get_mat_tol(wh)
+# print(f'дом построен? {foreman.build()}')
+
+d = {1: 2, 3: 4}
+d[5] = d.setdefault(5, 0) + 1
+print(d[5])
